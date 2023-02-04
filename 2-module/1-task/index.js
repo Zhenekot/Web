fed0d76ea47de28412db505164682dc293c0da56
Zhenekot <105406228+Zhenekot@users.noreply.github.com>
2023-02-04 18:57:42 +0500

    Add files via upload

---
+++ 2-module/1-task/index.js
@@ -0,0 +1,9 @@
+function sumSalary(salaries) {
+  let sum = 0;
+  for (let key in salaries) {
+      if(!Number.isNaN(salaries[key]) && Number.isFinite(salaries[key])){
+        sum += salaries[key];
+      }
+  }
+  return sum;
+}
