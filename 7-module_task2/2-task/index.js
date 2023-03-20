d8f3763003d0adb8714eed408562d435f8165c52
Zhenekot <105406228+Zhenekot@users.noreply.github.com>
2023-03-20 21:26:00 +0500

    Add files via upload

---
+++ 7-module_task2/2-task/index.js
@@ -0,0 +1,70 @@
+import createElement from '../../assets/lib/create-element.js';
+
+export default class Modal {
+
+  elem = null;
+  constructor() {
+    this.elem = this.render();
+    this.elem.querySelector(".modal__close").addEventListener("click", this.#buttonClose);
+    document.addEventListener("keydown", this.#onClickEsc);
+  }
+
+  open = () => {
+    document.body.classList.add("is-modal-open");
+    document.body.append(this.elem);
+  }
+
+  setTitle = (title) => {
+    this.elem.querySelector(".modal__title").textContent = title;
+  }
+
+  setBody = (node) => {
+    this.elem.querySelector(".modal__body").innerHTML = "";
+    this.elem.querySelector(".modal__body").append(node);
+  }
+
+  close = () => {
+    document.body.classList.remove("is-modal-open");
+    if (document.querySelector(".modal")) {
+      document.querySelector(".modal").remove();
+    }
+  }
+
+  #buttonClose = (event) => {
+    if (event.target.closest(".modal__close")) {
+      this.close();
+    }
+  }
+ 
+  #onClickEsc = (event) => {
+    if (event.code === 'Escape') {
+      this.close();
+    }
+  }
+  render() {
+    return createElement(`
+          <div class="modal">
+          <div class="modal__overlay"></div>
+
+          <div class="modal__inner">
+          <div class="modal__header">
+
+          <button type="button" class="modal__close">
+            <img src="../../assets/images/icons/cross-icon.svg" alt="close-icon" />
+          </button>
+
+          <h3 class="modal__title">
+            Вот сюда нужно добавлять заголовок
+          </h3>
+          </div>
+
+          <div class="modal__body">
+          A сюда нужно добавлять содержимое тела модального окна
+          </div>
+          </div>
+
+          </div>
+          `);
+  }
+
+}
