0bcc9ee0ad2a3cdaabf735980f227f3410822caa
Zhenekot <105406228+Zhenekot@users.noreply.github.com>
2023-04-02 19:42:17 +0500

    Add files via upload

---
+++ 8-module_1task/1-task/index.js
@@ -0,0 +1,68 @@
+import createElement from '../../assets/lib/create-element.js';
+
+export default class CartIcon {
+  #initialTopCoord = null;
+  #leftIndent = 0;
+  constructor() {
+    this.render();
+    this.addEventListeners();
+  }
+
+  render() {
+    this.elem = createElement('<div class="cart-icon"></div>');
+  }
+
+  update(cart) {
+    if (!cart.isEmpty()) {
+      this.elem.classList.add('cart-icon_visible');
+
+      this.elem.innerHTML = `
+        <div class="cart-icon__inner">
+          <span class="cart-icon__count">${cart.getTotalCount()}</span>
+          <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
+        </div>`;
+
+      this.updatePosition();
+
+      this.elem.classList.add('shake');
+      this.elem.addEventListener('transitionend', () => {
+        this.elem.classList.remove('shake');
+      }, { once: true });
+
+    } else {
+      this.elem.classList.remove('cart-icon_visible');
+    }
+  }
+
+  addEventListeners() {
+    document.addEventListener('scroll', () => this.updatePosition());
+    window.addEventListener('resize', () => this.updatePosition());
+  }
+
+  updatePosition() {
+    if (this.#initialTopCoord === null) {
+      this.#initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
+    }
+    if (window.pageYOffset > this.#initialTopCoord) {
+      this.#leftIndent = Math.min(
+        document.querySelector('.container').getBoundingClientRect().right + 20,
+        document.documentElement.clientWidth - this.elem.offsetWidth - 10
+      ) + 'px';
+      
+      Object.assign(this.elem.style, {
+        position: 'fixed',
+        top: '50px',
+        zIndex: 1e3,
+        right: '10px',
+        left: this.#leftIndent
+      });
+    } else if ((document.documentElement.clientWidth <= 767) || (window.pageYOffset < this.#initialTopCoord)) {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+      });
+    }
+  }
+}
